61f7603aee286197447a5f3260fccb1fd447de9f
Sam Bhadury <sam.bhaduri@insuremonkey.com>
2014-11-09 01:58:25 +0530

    login flow update

---
+++ js/custom/index.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function() {
+	//	load login settings on load
+	loadLoginSettings();
+
+	//	handle login action
+	jQuery('form#loginSettingsInit').submit(function() {
+		localStorage.setItem('host', jQuery(this).find('input[name="host"]').val());
+		localStorage.setItem('username', jQuery(this).find('input[name="username"]').val());
+		localStorage.setItem('password', jQuery(this).find('input[name="password"]').val());
+		
+		window.location = 'chat.html';
+		return false;
+	});
+});
+
+
+function loadLoginSettings() {
+	jQuery('form#loginSettingsInit	input[name="host"]').val(localStorage.getItem('host'));
+	jQuery('form#loginSettingsInit	input[name="username"]').val(localStorage.getItem('username'));
+	jQuery('form#loginSettingsInit	input[name="password"]').val(localStorage.getItem('password'));
+}
